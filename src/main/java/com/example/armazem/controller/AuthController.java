eb7263bea777963c9ca75bc002801088eb29b623
Ricardo Bessa <ricardobessa2021@gmail.com>
2026-09-06 21:22:10 -0300

    Adicionando controllers

---
+++ src/main/java/com/example/armazem/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.armazem.controller;
+
+import com.example.armazem.dto.LoginRequest;
+import com.example.armazem.dto.LoginResponse;
+import com.example.armazem.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@Valid @RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
---
+++ src/main/java/com/example/armazem/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.armazem.controller;
+
+import com.example.armazem.dto.CreateUserRequest;
+import com.example.armazem.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController{
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@Valid @RequestBody CreateUserRequest request) {
+        userService.create(request);
+    }
+}
